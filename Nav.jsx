de1d2f65230469502cccbe6f2472e8556b559c76
Nikita kanwar <nikita.ns321ns@gmail.com>
2025-01-26 13:01:54 +0530

    Create Nav.jsx

---
+++ Nav.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav className="container">
+    <div classsName="logo">
+      <img src="/images/watch.png" alt="logo"/>
+      </div>
+  
+  <ul>
+  <li href="#">Menu</li> 
+  <li href="#">Location</li> 
+  <li href="#">About</li> 
+  <li href="#">Contact</li> 
+    </ul>
+  
+    <button> Login</button>
+  
+  
+  </nav>  
+  )
+}
+
+export default Nav
