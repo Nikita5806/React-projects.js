398b7e1708e37c96d83f9362db04773cc7c3bc1e
Nikita kanwar <nikita.ns321ns@gmail.com>
2025-01-26 13:02:29 +0530

    Create Hero.jsx

---
+++ Hero.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <main className="hero container">
+        <div className="hero-content">
+            <h1>YOUR WRIST DESERVE THE BEST</h1>
+            <p>
+                YOUR WRIST DESERVE THE BEST AND WE ARE HERE TO HELP YOU WITH OUR WATCH.
+            </p>
+        </div>
+        <div className="hero-btn">
+            <button>Shop Now </button>
+            <button>Category</button>
+        </div>
+        <div className='brand-icons'>
+<img src="/images/amazon.png" alt="amflip"  />
+        </div>
+        <div className="shopping">
+            <p>Also Avaolable On</p>
+        </div>
+        <div className='hero-image'>
+            <img src="/images/bigw.png" alt="" />
+        </div>
+    </main>
+  )
+}
+
+export default Hero
