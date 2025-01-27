3e9ddaa4839e46d5a7aa6e6e1b245ce4d6e874bf
Nikita kanwar <nikita.ns321ns@gmail.com>
2025-01-27 23:26:25 +0530

    Create paste-app.js

---
+++ paste-app.js
@@ -0,0 +1,40 @@
+import { configureStore } from '@reduxjs/toolkit'
+import pasteReducer from './redux/pasteSlice'
+
+export default configureStore({
+  reducer: {
+    paste : pasteReducer,
+  },
+})
+
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState ={
+pastes:localStorage.getItem("pastes")
+? JSON.parse(localStorage.getItem)
+("pastes"):[]
+
+}
+
+export const pasteSlice = createSlice({
+  name: 'pastes',
+  initialState: 
+  reducers: {
+ addToPastes: (state, action) => {
+     
+ },
+    UpdateToPastes: (state, action) =>{ 
+
+    },
+resetAllPastes:(states, action ) =>{
+
+},
+   removeFromPastes : (state,action)  =>{
+
+   },
+  },
+})
+// Action creators are generated for each case reducer function
+export const {addToPastes ,UpdateToPastes , resetAllPastes, removeFromPaste } = counterSlice.actions
+
+export default pasteSlice.reducer
