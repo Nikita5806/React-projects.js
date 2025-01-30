ae8e72c3c54edd28b4e432363d5f552417738f06
Nikita kanwar <nikita.ns321ns@gmail.com>
2025-01-30 22:48:40 +0530

    Create gifhy.js

---
+++ gifhy.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import {RouterProvider, createBrowserRouter} from "react-router-dom";
+import Category from '../public/folder/Category';
+import Search from '../public/folder/Search';
+import Singlegif from '../public/folder/singlegif';
+import Fav from '../public/folder/Fav';
+import Home from '../public/folder/Home';
+import AppLayout from './Layout/Applayout';
