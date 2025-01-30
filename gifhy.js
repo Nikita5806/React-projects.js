5bc2c4cc74ecdc2cbacd45283b4703ef317762ad
Nikita kanwar <nikita.ns321ns@gmail.com>
2025-01-30 22:52:51 +0530

    Update gifhy.js

--- gifhy.js
+++ gifhy.js
@@ -72,3 +72,15 @@ const AppLayout = () => {
 }
 
 export default AppLayout
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
