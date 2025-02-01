b9e1e93a9294e54fdce8345e1fe676c578fedb50
Nikita kanwar <nikita.ns321ns@gmail.com>
2025-02-01 15:55:22 +0530

    Update gifhy.js

--- gifhy.js
+++ gifhy.js
@@ -197,3 +197,13 @@ const AppLayout = () => {
 
 export default AppLayout;
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./App"; // Ensure correct import
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
+
+
