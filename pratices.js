c3cd6ddb0844043f8445d12bcee349c45e38cfbb
Nikita kanwar <nikita.ns321ns@gmail.com>
2025-02-04 22:20:45 +0530

    Update pratices.js

--- pratices.js
+++ pratices.js
@@ -21,3 +21,16 @@ const JobList = ({ jobs }) => {
 };
 
 export default JobList;
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
